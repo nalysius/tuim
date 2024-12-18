8e585c8b68f7b0ae8395f43c1b2a4ee9bbe8f4bb
Anthony BOCCI <anthony.bocci@protonmail.com>
2024-12-18 11:02:15 +0100

    Container is now a UIElement
    Moreover, the methods related to border and padding have been moved from
    TableTrait to UIElement.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -7,23 +7,47 @@ pub mod table;
 pub trait UIElement {
 	/// Returns the z index of the UI element.
 	///
-	/// By default, all elements have the same basic index.
 	/// Elements with a greater z index are printed after
 	/// the the ones with a lower index. This way, it's possible for
 	/// an element to be printed above the others (e.g. Popup).
 	fn z_index(&self) -> u8 {
-		return 0;
+		0
 	}
+
+	/// Sets the z index of the element.
+	fn set_z_index(&mut self, z_index: u8);
+
 	/// Returns the title of the UI element.
 	///
 	/// By default the title is empty.
 	fn title(&self) -> &str {
 		""
 	}
+
+	/// Sets the title.
+	fn set_title(&mut self, title: &str);
 	/// Returns the width of the element.
 	fn width(&self) -> Size;
+	/// Sets the width.
+	fn set_width(&mut self, width: Size);
 	/// Returns the height of the element.
 	fn height(&self) -> Size;
+	/// Sets the height.
+	fn set_height(&mut self, height: Size);
+	/// Returns whether the element has been updated and needs to be redrawn.
+	fn updated(&self) -> bool;
+	/// Changes the update state of the element.
+	fn set_updated(&mut self, updated: bool);
+	/// Returns the vertical border character.
+	fn border_vertical(&self) -> char;
+	/// Returns the horizontal border character.
+	fn border_horizontal(&self) -> char;
+	/// Returns the intersection border character.
+	fn border_intersect(&self) -> char;
+	/// Returns the vertical padding.
+	fn padding_vertical(&self) -> u8;
+	/// Returns the horizontal padding.
+	fn padding_horizontal(&self) -> u8;
 }
 
 /// Position specifies the type of positioning used for an element.
@@ -49,6 +73,7 @@ pub enum Size {
 /// Represents a layout.
 ///
 /// A layout is a way to display the UI elements in a container.
+#[derive(Clone)]
 pub enum Layout {
 	/// Horizontal
 	/// Left to right
@@ -62,10 +87,133 @@ pub enum Layout {
 
 /// A container for UI elements.
 pub struct Container {
+	/// The z index of the element.
+	z_index: u8,
+	/// The title of the container.
+	title: String,
+	/// The width of the container.
+	width: Size,
+	/// The height of the container.
+	height: Size,
+	/// Whether the container has been updated.
+	updated: bool,
+	/// The character to display in vertical border.
+	/// Example: |
+	border_vertical: char,
+	/// The character to display in horizontal border.
+	/// Example: -
+	border_horizontal: char,
+	/// The character to display in border intersection.
+	/// Example: +
+	border_intersect: char,
+	/// The vertical space between the border and the text inside the table.
+	padding_vertical: u8,
+	/// The horizontal space between the border and the text inside the table.
+	padding_horizontal: u8,
 	/// The layout.
 	///
 	/// The elements will be displayed using this layout.
 	pub layout: Layout,
 	/// The UI elements to display.
 	pub elements: Vec<Box<dyn UIElement>>,
+}
+
+impl Container {
+	/// Creates a new Container.
+	///
+	/// # Parameters
+	/// - elements: the UI elements to store. The value is moved, to avoid cloning.
+	/// - layout: the layout of the container.
+	pub fn new(elements: Vec<Box<dyn UIElement>>, layout: Layout) -> Self {
+		Self {
+			z_index: 0,
+			title: "".to_string(),
+			width: Size::Auto,
+			height: Size::Auto,
+			updated: true,
+			border_vertical: ' ',
+			border_horizontal: ' ',
+			border_intersect: ' ',
+			padding_vertical: 0,
+			padding_horizontal: 0,
+			elements: elements,
+			layout: layout.clone()
+		}
+	}
+}
+
+impl UIElement for Container {
+	/// Returns the z index.
+	fn z_index(&self) -> u8 {
+		self.z_index
+	}
+
+	/// Sets the z index.
+	///
+	/// TODO: set the z index on all elements?
+	fn set_z_index(&mut self, z_index: u8) {
+		self.z_index = z_index;
+	}
+
+	/// Returns the title of the table.
+	fn title(&self) -> &str {
+		self.title.as_str()
+	}
+
+	/// Sets the title.
+	fn set_title(&mut self, title: &str) {
+		self.title = title.to_string();
+	}
+
+	/// Returns the width of the table.
+	fn width(&self) -> Size {
+		self.width.clone()
+	}
+
+	/// Sets the width.
+	fn set_width(&mut self, width: Size) {
+		self.width = width.clone();
+	}
+
+	/// Returns the height of the table.
+	fn height(&self) -> Size {
+		self.height.clone()
+	}
+
+	/// Sets the height.
+	fn set_height(&mut self, height: Size) {
+		self.height = height.clone();
+	}
+
+	/// Returns whether the table has been updated and
+	/// needs to be redrawn.
+	fn updated(&self) -> bool {
+		self.updated
+	}
+
+	/// Changes the state of the element.
+	fn set_updated(&mut self, updated: bool) {
+		self.updated = updated;
+	}
+
+	/// Returns the vertical border character.
+	fn border_vertical(&self) -> char {
+		self.border_vertical
+	}
+	/// Returns the horizontal border character.
+	fn border_horizontal(&self) -> char {
+		self.border_horizontal
+	}
+	/// Returns the intersection border character.
+	fn border_intersect(&self) -> char {
+		self.border_intersect
+	}
+	/// Returns the vertical padding.
+	fn padding_vertical(&self) -> u8 {
+		self.padding_vertical
+	}
+	/// Returns the horizontal padding.
+	fn padding_horizontal(&self) -> u8 {
+		self.padding_horizontal
+	}
 }
--- src/components/table.rs
+++ src/components/table.rs
@@ -1,5 +1,4 @@
 /// This module contains the definition of the Table UI element.
-/// TODO: implement Cell? Line?
 
 use super::{UIElement, Size};
 
@@ -17,20 +16,20 @@ pub trait TableTrait {
 	fn headers(&self) -> Vec<String>;
 	/// Returns the lines of data.
 	fn data(&self) -> &Vec<Vec<String>>;
-	/// Returns the vertical border character.
-	fn border_vertical(&self) -> char;
-	/// Returns the horizontal border character.
-	fn border_horizontal(&self) -> char;
-	/// Returns the intersection border character.
-	fn border_intersect(&self) -> char;
-	/// Returns the vertical padding.
-	fn padding_vertical(&self) -> u8;
-	/// Returns the horizontal padding.
-	fn padding_horizontal(&self) -> u8;
+	/// Returns the current page of the table.
+	fn current_page(&self) -> u32;
+	/// Sets the current page of the table.
+	fn set_current_page(&mut self, current_page: u32);
+	/// Returns the number of items displayed by page.
+	fn items_by_page(&self) -> u32;
+	/// Sets the number of items displayed by page.
+	fn set_items_by_page(&mut self, items_by_page: u32);
 }
 
 /// Represents the UI element Table.
 pub struct Table {
+	/// The z index.
+	z_index: u8,
 	/// The line of headers.
 	headers: Vec<String>,
 	/// The lines & columns contained in the table.
@@ -55,22 +54,32 @@ pub struct Table {
 	width: Size,
 	/// The height of the table.
 	height: Size,
+	/// Whether the table has been updated.
+	updated: bool,
+	/// The current page in the table.
+	current_page: u32,
+	/// The number of items displayed by page.
+	items_by_page: u32
 }
 
 impl TableTrait for Table {
 	/// Create a new Table.
 	fn new(headers: &[String], data: Vec<Vec<String>>) -> Self {
 		Self {
+			z_index: 0,
 			headers: headers.to_vec(),
 			data: data.to_vec(),
-			border_vertical: '|',
-			border_horizontal: '-',
-			border_intersect: '+',
+			border_vertical: ' ',
+			border_horizontal: ' ',
+			border_intersect: ' ',
 			padding_vertical: 1,
 			padding_horizontal: 1,
 			title: String::new(),
 			width: Size::Auto,
 			height: Size::Auto,
+			updated: true,
+			current_page: 0,
+			items_by_page: 20,
 		}
 	}
 
@@ -83,6 +92,80 @@ impl TableTrait for Table {
 	fn data(&self) -> &Vec<Vec<String>> {
 		&self.data
 	}
+
+	/// Returns the current page of the table.
+	fn current_page(&self) -> u32 {
+		self.current_page
+	}
+
+	/// Sets the current page of the table.
+	fn set_current_page(&mut self, current_page: u32) {
+		self.current_page = current_page
+	}
+
+	/// Returns the number of items displayed by page.
+	fn items_by_page(&self) -> u32 {
+		self.items_by_page
+	}
+
+	/// Sets the number of items displayed by page.
+	fn set_items_by_page(&mut self, items_by_page: u32) {
+		self.items_by_page = items_by_page
+	}
+}
+
+impl UIElement for Table {
+	/// Returns the z index.
+	fn z_index(&self) -> u8 {
+		self.z_index
+	}
+
+	/// Sets the z index.
+	fn set_z_index(&mut self, z_index: u8) {
+		self.z_index = z_index;
+	}
+	
+	/// Returns the title of the table.
+	fn title(&self) -> &str {
+		self.title.as_str()
+	}
+
+	/// Sets the title.
+	fn set_title(&mut self, title: &str) {
+		self.title = title.to_string();
+	}
+
+	/// Returns the width of the table.
+	fn width(&self) -> Size {
+		self.width.clone()
+	}
+
+	/// Sets the width.
+	fn set_width(&mut self, width: Size) {
+		self.width = width.clone();
+	}
+
+	/// Returns the height of the table.
+	fn height(&self) -> Size {
+		self.height.clone()
+	}
+
+	/// Sets the height.
+	fn set_height(&mut self, height: Size) {
+		self.height = height.clone();
+	}
+
+	/// Returns whether the table has been updated and
+	/// needs to be redrawn.
+	fn updated(&self) -> bool {
+		self.updated
+	}
+
+	/// Changes the state of the element.
+	fn set_updated(&mut self, updated: bool) {
+		self.updated = updated;
+	}
+
 	/// Returns the vertical border character.
 	fn border_vertical(&self) -> char {
 		self.border_vertical
@@ -102,22 +185,5 @@ impl TableTrait for Table {
 	/// Returns the horizontal padding.
 	fn padding_horizontal(&self) -> u8 {
 		self.padding_horizontal
-	}
-}
-
-impl UIElement for Table {
-	/// Returns the title of the table.
-	fn title(&self) -> &str {
-		self.title.as_str()
-	}
-
-	/// Returns the width of the table.
-	fn width(&self) -> Size {
-		self.width.clone()
-	}
-
-	/// Returns the height of the table.
-	fn height(&self) -> Size {
-		self.height.clone()
 	}
 }
